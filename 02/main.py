f158571612f17049fa4db754b78930a55ab9c2f6
Oleksandr Danyliuk1 <oleksandr_danyliuk1@epam.com>
2026-08-19 12:39:08 +0300

    ht2, task 4-7

--- 02/main.py
+++ 02/main.py
@@ -14,3 +14,33 @@
 # result_minutes = minutes % 60
 # print(result_hours, 'hours' , result_minutes, 'minutes.')
 
+# 4.
+# price = float(input("Enter the price: "))
+# discount = float(input("Enter the discount percentage: "))
+# final_price = price - (price * discount / 100)
+# print('Final price after discount:, final_price)
+
+# 5.
+# number = int(input("Enter a number: "))
+# last_digit = number % 10
+# print('Last digit of the number:', last_digit)
+
+
+# 6.
+# length = float(input("Enter the length: "))
+# width = float(input("Enter the width: "))
+# perimeter = 2 * (length + width)
+# print('Perimeter of the rectangle:', perimeter)
+
+# 7.
+# number = int(input("Enter a four-digit number: "))
+
+# digit1 = number // 1000
+# digit2 = (number // 100) % 10
+# digit3 = (number // 10) % 10
+# digit4 = number % 10
+
+# print(digit1)
+# print(digit2)
+# print(digit3)
+# print(digit4)
