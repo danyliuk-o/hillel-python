9153614facb561bfd4393a73496e3f10ada6f1e8
Oleksandr Danyliuk1 <oleksandr_danyliuk1@epam.com>
2026-08-19 12:10:20 +0300

    ht2, task 1-3

---
+++ 02/main.py
@@ -0,0 +1,16 @@
+# 1.
+# user_number = int(input("Enter a number: "))
+# print(user_number**2)
+
+# 2.
+# user_number1 = int(input("Enter a number 1: "))
+# user_number2 = int(input("Enter a number 2: "))
+# user_number3 = int(input("Enter a number 3: "))
+# print('Average value:', (user_number1 + user_number2 + user_number3) / 3)
+
+# 3.
+# minutes = int(input("Enter the number of minutes: "))
+# result_hours = minutes // 60
+# result_minutes = minutes % 60
+# print(result_hours, 'hours' , result_minutes, 'minutes.')
+
