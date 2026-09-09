caddd1dcee6e3f78d90f9281922edbee6d0994e7
Oleksandr Danyliuk1 <oleksandr_danyliuk1@epam.com>
2026-09-09 10:04:50 +0300

    hw7, task2, tests

--- 07/7.2/main.py
+++ 07/7.2/main.py
@@ -5,4 +5,9 @@ def correct_sentence(text):
         text += "."
     return text
 
-print(correct_sentence("greetings, friends."))
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
