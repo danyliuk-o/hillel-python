2519ba612d8f04870987be27691ce35b62fda3de
Oleksandr Danyliuk1 <oleksandr_danyliuk1@epam.com>
2026-09-13 15:24:38 +0300

    hw8, task2

---
+++ 08/8.2/main.py
@@ -0,0 +1,10 @@
+def is_palindrome(text):
+    text = "".join(i for i in text.lower() if i.isalnum())
+    return text == text[::-1]
+
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
